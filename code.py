564c45579423ce0b6d6ae3f4060529382cd97bf8
Fábio Alves <fabioalves.contato@gmail.com>
2021-09-17 13:19:18 -0100

    refactor:  clean the code:

--- code.py
+++ code.py
@@ -1,17 +1,17 @@
 import os
 
+def open(args):
+    path = args.open[0]
+    os.system(f'code {path}')
+
 def add_subcommand_code(subparsers):
     parser = subparsers.add_parser('code')
-    # parser.set_defaults(func=hello)
 
     parser.add_argument("-o","--open", type = str, nargs = 1,
         metavar = "", default = ".",
         help = "Open a VSCode folder")
-    # parser.add_argument("-o","--open", type = str)
 
     args = parser.parse_args()
-
-
-def open(args):
-    path = args.open[0]
-    os.system(f'code {path}')
+    print(args)
+    if args.open != None:
+        open(args)
--- main.py
+++ main.py
@@ -2,36 +2,15 @@ import argparse
 import os
 
 
-def main(args=None):
-  parser = argparse.ArgumentParser(prog="FSK CLI", description = "A CLI by FSK!")
+def main():
+  parser = argparse.ArgumentParser(prog="fsk", description = "A CLI by FSK!")
 
-  # parser.add_argument("-co","--code", type = str, nargs = 1,
-  #                       metavar = "", default = None,
-  #                       help = "Control VSCode using the CLI.")
+  subparsers = parser.add_subparsers()
 
-  parser.add_argument("-o","--open", type = str, nargs = 1,
-        metavar = "", default = ".",
-        help = "Open a VSCode folder")
-
-  # subparsers = parser.add_subparsers()/
-
-  # from code import add_subcommand_code
-  # add_subcommand_code(subparsers)
-
-  # parse the arguments from standard input
-  args = parser.parse_args(args)
-  # print("S" , args)
-    
-  # calling functions depending on type of argument
-  if args.open != None:
-    open(args)
-
-def open(args):
-    path = args.open[0]
-    os.system(f'code {path}')
+  from code import add_subcommand_code
+  add_subcommand_code(subparsers)
 
 
 if __name__ == "__main__":
     # calling the main function
     main()
-
