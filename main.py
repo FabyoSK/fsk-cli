f53113ddd787f2cd7421a078388faa702f72b676
Fábio Alves <fabioalves.contato@gmail.com>
2021-09-17 13:00:23 -0100

    feat: add -o --open to open a vscode instance

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import argparse
+import os
 
-parser = argparse.ArgumentParser(description='Add some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='interger list')
-parser.add_argument('--sum', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
-args = parser.parse_args()
 
-print(args.sum(args.integers))
+def main(args=None):
+  parser = argparse.ArgumentParser(prog="FSK CLI", description = "A CLI by FSK!")
 
-  
+  # parser.add_argument("-co","--code", type = str, nargs = 1,
+  #                       metavar = "", default = None,
+  #                       help = "Control VSCode using the CLI.")
+
+  parser.add_argument("-o","--open", type = str, nargs = 1,
+        metavar = "", default = ".",
+        help = "Open a VSCode folder")
+
+  # subparsers = parser.add_subparsers()/
+
+  # from code import add_subcommand_code
+  # add_subcommand_code(subparsers)
+
+  # parse the arguments from standard input
+  args = parser.parse_args(args)
+  # print("S" , args)
+    
+  # calling functions depending on type of argument
+  if args.open != None:
+    open(args)
+
+def open(args):
+    path = args.open[0]
+    os.system(f'code {path}')
+
+
+if __name__ == "__main__":
+    # calling the main function
+    main()
+
